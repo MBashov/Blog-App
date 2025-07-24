481e819093d19c435c8c5ba0a03ab4d3e98f0d5c
MBashov <galacticos.mb@gmail.com>
2025-07-24 16:52:17 +0300

    About page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './layout/home/home';
-import { BlogTem } from './featured/blog-tem/blog-tem';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'blogs', loadComponent: () => import('./featured/blog-tem/blog-tem').then(c => c.BlogTem) },
+    { path: 'about', loadComponent: () => import('./featured/about/about').then(c => c.About) },
 ];
