565a86eae539fb025619a00991d3016153bea007
MBashov <galacticos.mb@gmail.com>
2025-07-24 18:44:59 +0300

    Register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { Home } from './layout/home/home';
 export const routes: Routes = [
     { path: 'home', component: Home },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'blogs', loadComponent: () => import('./featured/blog-tem/blog-tem').then(c => c.BlogTem) },
-    { path: 'login', loadComponent: () => import('./featured/auth/login/login').then(c => c.Login) },
-    { path: 'about', loadComponent: () => import('./featured/about/about').then(c => c.About) },
+    { path: 'blogs', loadComponent: () => import('./features/blog-tem/blog-tem').then(c => c.BlogTem) },
+    { path: 'login', loadComponent: () => import('./features/auth/login/login').then(c => c.Login) },
+    { path: 'register', loadComponent: () => import('./features/auth/register/register').then(c => c.Register) },
+    { path: 'about', loadComponent: () => import('./features/about/about').then(c => c.About) },
 ];
