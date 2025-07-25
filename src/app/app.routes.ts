fea473f2241e28dbab83bd5ff58f299f63b6a670
MBashov <galacticos.mb@gmail.com>
2025-07-25 10:11:56 +0300

    Add Post component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,5 @@ export const routes: Routes = [
     { path: 'login', loadComponent: () => import('./features/auth/login/login').then(c => c.Login) },
     { path: 'register', loadComponent: () => import('./features/auth/register/register').then(c => c.Register) },
     { path: 'about', loadComponent: () => import('./features/about/about').then(c => c.About) },
+    { path: 'add-blog', loadComponent: () => import('./features/create-blog/create-blog').then(c => c.CreateBlog) },
 ];
