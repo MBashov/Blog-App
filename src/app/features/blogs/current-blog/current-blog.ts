61d6372b6067ec329fa224f1e02df672690139d1
MBashov <galacticos.mb@gmail.com>
2025-07-28 13:37:53 +0300

    Show blog images in modal

--- src/app/features/blogs/current-blog/current-blog.ts
+++ src/app/features/blogs/current-blog/current-blog.ts
@@ -15,6 +15,8 @@ export class CurrentBlog implements OnInit {
     protected blog = {} as Blog;
     protected isLoading: boolean = true;
     protected imageClass: string = '';
+    protected modalClass: string = '';
+    protected imageSrc: string = '';
 
     constructor(private route: ActivatedRoute, private apiService: ApiService) { }
 
@@ -24,19 +26,29 @@ export class CurrentBlog implements OnInit {
         this.apiService.getSingleBLog(slug).subscribe(response => {
             this.blog = response.blog;
         });
-        
+
         this.isLoading = false;
     }
 
-    protected omImageLoad(event: Event) {
+    protected omImageLoad(event: Event): void {
         const img = event.target as HTMLImageElement;
-            
-        const {width, height} = img; 
-        
+
+        const { width, height } = img;
+
         if (width > height) {
             this.imageClass = 'landscape';
         } else {
             this.imageClass = 'portrait';
         }
+    }
+
+    protected showModal(event: Event): void {
+        const { src } = event.target as HTMLImageElement;
+        this.imageSrc = src;
+        this.modalClass = 'active';
+    }
+
+    protected closeModal(): void {
+        this.modalClass = '';
     }
 }
